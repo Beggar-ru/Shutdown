676173d2f6d15a72d793cd52f55ec8ae83b33e90
Beggar <75258613+Beggar-ru@users.noreply.github.com>
2025-01-16 20:23:02 +0300

    beta

--- turn off timer.cpp
+++ turn off timer.cpp
@@ -4,8 +4,34 @@
 #include <windows.h>
 #include <chrono>
 #include <thread>
+#include <unordered_map>
+#include <fstream>
 
 using namespace std;
+
+unordered_map<string, string> loadTranslations(const string& lang) {
+    unordered_map<string, string> translations;
+    ifstream file(lang);
+    string line;
+
+    while (getline(file, line)) {
+        size_t pos = line.find('=');
+        if (pos != string::npos) {
+            string key = line.substr(0, pos);
+            string value = line.substr(pos + 1);
+            translations[key] = value;
+        }
+    }
+    return translations;
+}
+
+string translate(const unordered_map<string, string>& translations, const string& msg){
+    auto it = translations.find(msg);
+    if (it != translations.end()) {
+        return it->second;
+    }
+    return msg;
+}
 
 void shutdownPC(int seconds) {
     cout << "Выключение через: " << seconds << endl;
@@ -13,9 +39,24 @@ void shutdownPC(int seconds) {
     system(commandOFF.c_str());
 }
 
-int main()
-{
+int main() {
     setlocale(LC_ALL, "ru");
+
+    string language = determineLanguage();
+    unordered_map<string, string> translations;
+
+    if (langauge == "ru") ||
+    (language == "uk") || 
+    (language == "bk") ||
+    (language == "kk") {
+        translations = loadTranslations("ru_RU.txt");
+    }
+    else {
+        tranlations = loadTranslations("en_US.txt");
+    }
+
+    cout << translate(translations, "TURN_OFF") << endl;
+    
 
     int minuts = 0, seconds = 0;
 
